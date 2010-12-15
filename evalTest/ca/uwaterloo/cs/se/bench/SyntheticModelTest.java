b897bcf24ebe4721f9fb1a62cbc34d760c021788
reid holmes <rtholmes@gmail.com>
2010-12-15 13:38:48 -0500

    rename dummy -> synthetic

---
+++ evalTest/ca/uwaterloo/cs/se/bench/SyntheticModelTest.java
@@ -0,0 +1,37 @@
+package ca.uwaterloo.cs.se.bench;
+
+import java.util.Collection;
+
+import junit.framework.Assert;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import ca.uwaterloo.cs.se.bench.model.ClassElement;
+import ca.uwaterloo.cs.se.bench.model.io.XMLReaderDependencyFinder;
+
+/**
+ * 
+ * @author rtholmes
+ *
+ */
+public class SyntheticModelTest {
+
+	private final String fName = "testResources/static_synthetic.xml";
+
+	@BeforeClass
+	public static void beforeClass() {
+		XMLReaderDependencyFinder.startLog4J(true);
+	}
+
+	@Test
+	public void parseSytheticModel() {
+
+		XMLReaderDependencyFinder xmlrdf = new XMLReaderDependencyFinder(fName);
+		Collection<ClassElement> model = xmlrdf.parseModel();
+
+		Assert.assertNotNull(model);
+	}
+	
+	
+}
