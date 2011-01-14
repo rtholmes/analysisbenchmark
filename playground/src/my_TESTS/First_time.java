93ba4297a55f6c87f504f52c0f44ffa15423c6e2
kimiisa <kimiisa@localhost>
2011-01-14 15:05:54 -0500

    This is a folder where I have my test cases/classfiles

---
+++ playground/src/my_TESTS/First_time.java
@@ -0,0 +1,33 @@
+package my_TESTS;
+
+
+//This is my first interface
+interface First_interface {
+	
+}
+
+//This is my second interface
+interface Second_interface { }
+
+//This will be the first super class
+class First_Parent {
+
+public void a() {}
+
+}
+
+//This will be the child class
+ class First_child extends First_Parent implements First_interface, Second_interface {
+	public void b() {
+		a();
+	}
+	
+	//Need main to run
+    public static void main(String[] args) {
+        System.out.println("Hello World!"); 
+    }
+
+	
+}
+ 
+ 
