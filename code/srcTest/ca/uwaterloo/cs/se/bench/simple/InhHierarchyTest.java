00c2124203567fe181b0d274fdcf945622ad602a
reid holmes <rtholmes@gmail.com>
2011-02-08 13:48:42 -0500

    add initial test for the inh hierarchy; this isn't complete but it's enough for now.

---
+++ code/srcTest/ca/uwaterloo/cs/se/bench/simple/InhHierarchyTest.java
@@ -0,0 +1,57 @@
+package ca.uwaterloo.cs.se.bench.simple;
+
+import org.junit.Test;
+
+/**
+ * This isn't complete but is a sufficient start.
+ * 
+ * @author rtholmes
+ * 
+ */
+public class InhHierarchyTest {
+	@Test
+	public void createADirect() {
+		InhClassA ica = new InhClassA();
+	}
+
+	@Test
+	public void createAInDirect() {
+		InhInterface ica = new InhClassA();
+	}
+
+	@Test
+	public void createAAbstract() {
+		InhAbstractClass ica = new InhClassA();
+	}
+
+	@Test
+	public void callInterfaceADirect() {
+		InhClassA ica = new InhClassA();
+		ica.interfaceMethodA();
+	}
+
+	@Test
+	public void callInterfaceAInDirect() {
+		InhInterface ica = new InhClassA();
+		ica.interfaceMethodA();
+	}
+
+	@Test
+	public void callInterfaceAAbstract() {
+		InhAbstractClass ica = new InhClassA();
+		ica.interfaceMethodA();
+	}
+
+	@Test
+	public void callAbsADirect() {
+		InhClassA ica = new InhClassA();
+		ica.abstractMethodA();
+	}
+
+	@Test
+	public void callAbsAInDirect() {
+		InhAbstractClass ica = new InhClassA();
+		ica.abstractMethodA();
+	}
+
+}
