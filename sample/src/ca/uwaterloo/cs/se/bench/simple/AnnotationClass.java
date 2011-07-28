6c8a732ab36e8d31b155978661932efc340622df
Reid Holmes <rtholmes@gmail.com>
2011-07-28 14:04:36 -0700

    annotation updates; it turns out @SuppressWarnings have a retention policy that doesn't leave them in the code.

--- sample/src/ca/uwaterloo/cs/se/bench/simple/AnnotationClass.java
+++ sample/src/ca/uwaterloo/cs/se/bench/simple/AnnotationClass.java
@@ -2,7 +2,11 @@ package ca.uwaterloo.cs.se.bench.simple;
 
 import org.junit.Test;
 
+@Deprecated
 public class AnnotationClass {
+
+	@Deprecated
+	Class f;
 
 	@Deprecated
 	public void hasJDKAnnotation() {
@@ -15,6 +19,7 @@ public class AnnotationClass {
 	}
 
 	// NOTE: Do NOT remove these warnings; they are for testing
+	// NOTE: @Suppresswarnings is only a source annotation, it does not get compiled so these don't get picked up by depfind
 	@SuppressWarnings({ "deprecation", "unused" })
 	public void hadCompoundAnnotation() {
 
